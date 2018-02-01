22d0d3602a53c3aa9fd7e7bf9cef90aa8052e553
Dominique HAAS <contact@dominique-haas.fr>
2018-02-02 00:38:53 +0100

    fix description

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,11 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Copy & Past your Emojis !" 😀😁😂🤣😃😄😅😆😉}
+      {
+        hid: "description",
+        name: "description",
+        content: "Copy & Past your Emojis ! 😀😁😂🤣😃😄😅😆😉"
+      }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
