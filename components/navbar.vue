f75e944390060c84fcc5c6c54af8311cd7c66277
Dominique HAAS <contact@dominique-haas.fr>
2018-02-05 00:39:47 +0100

    add twitter

--- components/navbar.vue
+++ components/navbar.vue
@@ -3,7 +3,7 @@
     <div class="navbar-brand">
 
       <router-link class="navbar-item" to="/" rel="home">
-      {{randomEmoji}}
+        {{randomEmoji}}
       </router-link>
 
       <div id="navbarBurger" class="navbar-burger burger is-unselectable" data-target="navMenuDocumentation">
@@ -21,20 +21,43 @@
         </router-link>
 
       </div>
+      <div class="navbar-end">
+        <a class="navbar-item is-hidden-desktop-only" href="https://twitter.com/emoji_ovh" target="noopenner">
 
+          <svg class="twitter-icon" viewBox="328 355 335 276" xmlns="http://www.w3.org/2000/svg">
+            <path d="
+    M 630, 425
+    A 195, 195 0 0 1 331, 600
+    A 142, 142 0 0 0 428, 570
+    A  70,  70 0 0 1 370, 523
+    A  70,  70 0 0 0 401, 521
+    A  70,  70 0 0 1 344, 455
+    A  70,  70 0 0 0 372, 460
+    A  70,  70 0 0 1 354, 370
+    A 195, 195 0 0 0 495, 442
+    A  67,  67 0 0 1 611, 380
+    A 117, 117 0 0 0 654, 363
+    A  65,  65 0 0 1 623, 401
+    A 117, 117 0 0 0 662, 390
+    A  65,  65 0 0 1 630, 425
+    Z" style="fill:#3BA9EE;" />
+          </svg>
+
+        </a>
+      </div>
     </div>
   </nav>
 </template>
 <script>
 export default {
   computed: {
-    randomEmoji() {
+    randomEmoji () {
       const emojis = ["😀", "😁", "😂", "🤣", "😃", "😄", "😅", "😆", "😉"];
 
       return emojis[Math.floor(Math.random() * emojis.length)];
     }
   },
-  mounted() {
+  mounted () {
     const $navbarBurgers = Array.prototype.slice.call(
       document.querySelectorAll(".navbar-burger"),
       0
@@ -73,5 +96,10 @@ export default {
   display: -ms-flexbox;
   display: flex;
   justify-content: center;
+}
+
+.twitter-icon {
+  display: block;
+  height: 24px;
 }
 </style>
