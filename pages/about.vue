e695a1f6d70b9aebdf990155ff7e6108a1087852
Dominique HAAS <contact@dominique-haas.fr>
2018-02-05 14:28:41 +0100

    add about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,63 @@
 <template>
-  <div class="container">
+  <div>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h2 class="title is-2">
+            About
+          </h2>
+
+        </div>
+      </div>
+    </section>
+
+    <div class="container">
+      <section class="section has-text-centered is-size-3">
+        <p>Emoji.ovh is a Side Project build with a lot of 😀😁😂🤣😃😄 by
+          <a href="https://dominique-haas.fr" target="noopenner">Dominique HAAS</a>
+        </p>
+      </section>
+      <section class="section">
+        <h3 class="title is-3">Build with</h3>
+        <div class="columns is-vcentered is-mobile has-text-centered">
+
+          <div class="column">
+            <img src="/nuxt-logo.svg" alt="Nuxt Logo" class="logo">
+          </div>
+          <div class="column">
+            <img src="/bulma-logo.svg" alt="Bulma Logo" class="logo">
+          </div>
+          <div class="column">
+            <img src="/algolia-logo.svg" alt="Algolia Logo" class="logo">
+          </div>
+
+        </div>
+      </section>
+      <section class="section">
+        <h3 class="title is-3">Hosted by</h3>
+        <div class="columns is-vcentered is-mobile has-text-centered">
+          <div class="column">
+            <img src="/netlify-logo.svg" alt="Netlify Logo" class="logo">
+          </div>
+        </div>
+      </section>
+      <section class="section">
+        <h3 class="title is-3">Thanks</h3>
+        <div class="is-size-4">
+          <ul>
+            <li>Nanα</li>
+          </ul>
+        </div>
+      </section>
+
+    </div>
 
   </div>
 </template>
+
+<style>
+.logo {
+  height: 80px;
+}
+</style>
+
